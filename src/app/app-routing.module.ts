a02a421ba9cd7707fbba9fd8087f66f51500a00e
Guilhermee19 <guilherme.santana1998@gmail.com>
2023-02-09 02:10:07 -0300

    feat: telas, components, variaveis scss e component do angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { ConfigComponent } from './pages/config/config.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: NavbarComponent,
+    children: [
+      { path: '', redirectTo: 'tasks', pathMatch: 'full' },
+      { path: 'tasks', component: TasksComponent, title: 'Tarefas' },
+      { path: 'calendar', component: CalendarComponent, title: 'Calendario' },
+      { path: 'config', component: ConfigComponent, title: 'Configuração' },
+    ]
+  },
+  { path: '**', redirectTo: 'tasks' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSidenavModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IconDarkLightComponent } from './components/icon-dark-light/icon-dark-light.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { ConfigComponent } from './pages/config/config.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { IconComponent } from './components/Icons/icons-svg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconDarkLightComponent,
+    IconComponent,
+    NavbarComponent,
+    TasksComponent,
+    ConfigComponent,
+    CalendarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
